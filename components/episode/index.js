ce3cfadeaf39acba7822f5377649ba52eddda1d9
cht <cht@senthink.com>
2018-11-27 17:16:06 +0800

    首页上三分之二的排版布局

--- components/episode/index.js
+++ components/episode/index.js
@@ -22,11 +22,19 @@ Component({
   data: {
     year: 0,
     month: '',
+    months: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
     _index: '',
   },
 
   attached: function() {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
 
+    this.setData({
+      year,
+      month: this.data.months[month]
+    })
   },
   /**
    * 组件的方法列表
