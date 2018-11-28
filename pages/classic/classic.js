990312dc07449c886bc6ec9ed273260b3f5184c0
cht <cht@senthink.com>
2018-11-28 16:44:00 +0800

    解决左上角“喜欢”的真实数据没有及时更新的问题

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -13,6 +13,8 @@ Page({
     classic: {},
     latest: true,
     first: false,
+    likeCount: 0,
+    likeStatus: false,
   },
 
   /**
@@ -22,6 +24,8 @@ Page({
     classicModel.getLatest((res)=>{
       this.setData({
         classic: res,
+        likeCount: res.fav_nums,
+        likeStatus: res.like_status,
       })  
     })
   },
@@ -43,6 +47,7 @@ Page({
   _updataClassic: function(nextOrPrevious){
     let index = this.data.classic.index
     classicModel.getClassic(index, nextOrPrevious, (res)=>{
+      this._getLikeStatus(res.id, res.type)
       this.setData({
         classic: res,
         latest: classicModel.isLatest(res.index),
@@ -50,52 +55,13 @@ Page({
       })
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
+  _getLikeStatus: function(artID, category) {
+    likeModel.getClassicLikeStatus(artID, category, (res)=>{
+      this.setData({
+        likeStatus: res.like_status,
+        likeCount: res.fav_nums,
+      })
+    })
+  }
 })
